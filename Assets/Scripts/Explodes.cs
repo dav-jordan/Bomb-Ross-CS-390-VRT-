3e25c0edfc4ab17db9e5f2cc7d40aba2b585d0af
Jordan <jorda128@purdue.edu>
2019-10-10 12:13:21 -0400

    updated scene and created explodable script for paint grenades

---
+++ Assets/Scripts/Explodes.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explodes : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag("ExplodesBomb"))
+            return;
+        Destroy(gameObject);
+
+        // TODO: Add code to splatter bomb
+    }
+}
