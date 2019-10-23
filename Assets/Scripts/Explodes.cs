f35d7cdd604658751a6cda72a116a5df9b87cd30
Jordan <jorda128@purdue.edu>
2019-10-22 20:37:22 -0400

    Add sound effects

--- Assets/Scripts/Explodes.cs
+++ Assets/Scripts/Explodes.cs
@@ -6,6 +6,7 @@ public class Explodes : MonoBehaviour
 {
     public Transform SpawnPosition;
     public GameObject splatter;
+
     void OnCollisionEnter(Collision collision)
     {
         if (!collision.gameObject.CompareTag("ExplodesBomb") && !collision.gameObject.CompareTag("Canvas"))
@@ -16,9 +17,11 @@ public class Explodes : MonoBehaviour
         {
             Instantiate(splatter, prevPosition.position, Quaternion.FromToRotation(Vector3.up, collision.gameObject.transform.rotation.eulerAngles));
         }
+
         this.transform.position = SpawnPosition.position;
         this.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
         this.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
 
     }
+
 }
